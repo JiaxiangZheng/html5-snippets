c0180cdfa992c292185213602762c15e53db3057
Jiaxiang Zheng <jiaxiang.zheng135@gmail.com>
2014-10-20 23:18:22 +0800

    add JSONP demo

--- README.md
+++ README.md
@@ -11,4 +11,5 @@ This repository contains my JS/HTML/CSS related code snippets for studying.
 * [继承机制](js/inherit.js)
 * [多重继承机制-支持super](js/mixin.js)
 * [JS对象构造的TAB选项卡](html/tab-switcher.html)
+* [JSONP实现查询12306火车票信息](html/JSONP.html)
 
---
+++ js/JSONP.js
@@ -0,0 +1,37 @@
+(function () {
+    function wrapQuery(query) {
+        var strs = []
+        for (var key in query) {
+            strs.push(key + "=" + query[key]);
+        }
+        return strs.join("&");
+    }
+    window.fnHandleRequest = function fnHandleRequest(response) {
+        if (!response.status) {
+            alert("failed to load data");
+            return
+        }
+        // response is the object created by the JSON
+        console.log("查询到" + response + "趟车次！");
+        alert(response.data.length);
+    }
+    function JSONP(_date, _from, _to) {
+        var script = document.createElement('script');
+        script.type = "text/javascript";
+        var query = {
+            date: _date,
+            from: _from,
+            to: _to,
+            callback: "fnHandleRequest"
+        };
+
+        script.src = "http://localhost:4000/query?" + wrapQuery(query);
+        document.body.appendChild(script);
+    }
+    var submit = document.querySelector("#query-submit");
+    submit.addEventListener('click', function (evt) {
+        var _date = document.querySelector("#query-date").value;
+        JSONP(_date, "HZH", "JJG");
+        evt.preventDefault();
+    });
+}());
